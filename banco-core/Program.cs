d3f03322f0f7fe2a42ef1906a67221351c0d47a7
Robert Morales <lsurec@demosoft.com.gt>
2024-10-14 12:19:14 -0600

    add usuario table

--- banco-core/Program.cs
+++ banco-core/Program.cs
@@ -1,3 +1,6 @@
+using banco_core.Utilities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Agregar contexto de la base de datos al contenedor de servicios
+builder.Services.AddDbContext<BancoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BancoDatabase")));
+
 
 var app = builder.Build();
 
---
+++ banco-core/Utilities/BancoContext.cs
@@ -0,0 +1,18 @@
+﻿using banco_core.Modelo;
+using Microsoft.EntityFrameworkCore;
+
+namespace banco_core.Utilities
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options) { }
+
+        // Define las tablas como propiedades DbSet
+        public DbSet<UsuarioModel> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Aquí puedes configurar relaciones o restricciones en las tablas
+        }
+    }
+}
