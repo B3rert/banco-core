32f6752422d0a78ec9fb4fc7c890d7911bd995d1
Robert Morales <lsurec@demosoft.com.gt>
2024-10-14 12:48:05 -0600

    add token

--- banco-core/Controllers/UsuarioController.cs
+++ banco-core/Controllers/UsuarioController.cs
@@ -3,6 +3,11 @@ using banco_core.Utilities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 
 namespace banco_core.Controllers
@@ -12,10 +17,14 @@ namespace banco_core.Controllers
     public class UsuarioController : ControllerBase
     {
         private readonly BancoContext _context;
+        private readonly IConfiguration _config;
 
-        public UsuarioController(BancoContext context)
+
+        public UsuarioController(BancoContext context, IConfiguration configuration)
         {
             _context = context;
+            _config = configuration;
+
         }
 
 
@@ -32,6 +41,32 @@ namespace banco_core.Controllers
                 // Si el usuario no existe o la contraseña es incorrecta, retorna 401 Unauthorized
                 return Unauthorized(new { message = "Credenciales incorrectas" });
             }
+
+
+
+            //Crear llave secreta para token
+            var secretKey = _config.GetValue<string>("SecretKey");
+            var key = Encoding.ASCII.GetBytes(secretKey);
+
+            //Agregar contenido token
+            var claims = new ClaimsIdentity();
+            claims.AddClaim(new Claim(ClaimTypes.NameIdentifier, loginRequest.User!));
+
+            //Configurar token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claims,
+                Expires = DateTime.UtcNow.AddDays(360),
+
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            //Crear token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var createdToken = tokenHandler.CreateToken(tokenDescriptor);
+            var barerToken = tokenHandler.WriteToken(createdToken);
 
             // Si todo es correcto, puedes devolver el usuario o un token de autenticación
             return Ok(new { message = "Login exitoso", usuario.Usuario, usuario.Rol_Id});
--- banco-core/Program.cs
+++ banco-core/Program.cs
@@ -1,5 +1,9 @@
 using banco_core.Utilities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +19,28 @@ builder.Services.AddDbContext<BancoContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("BancoDatabase")));
 
 
+//Llmar llave privada del archivo 
+var key = Encoding.ASCII.GetBytes(builder.Configuration.GetValue<string>("SecretKey"));
+
+//Configurar tokens para el rpyecto
+builder.Services.AddAuthentication(x => {
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x => {
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,9 +50,22 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+//Configurar cors
+app.UseCors(opctions =>
+{
+    opctions.WithOrigins("*");
+    opctions.AllowAnyMethod();
+    opctions.AllowAnyHeader();
+    opctions.WithMethods("PUT", "GET", "POST", "DELETE");
+});
+
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseAuthentication();
 
 app.MapControllers();
 
